30ba99661fbe8af83b1f40240a3f0cb84abe5b37
Teacher Luis <montanapawer@gmail.com>
2025-07-03 11:06:50 -0400

    Agregue cards en mision, vision y valores

--- js/institucion.js
+++ js/institucion.js
@@ -1,4 +1,14 @@
 // js/institucion.js
 window.initInstitucion = function() {
-  // Bootstrap tabs ya funcionan por data-bs-toggle, pero puedes poner lógica extra aquí si quieres
+  // Forzar la inicialización de los tabs de Bootstrap
+  if (window.bootstrap && window.bootstrap.Tab) {
+    var tabElList = [].slice.call(document.querySelectorAll('#institucionTabs button[data-bs-toggle="tab"]'));
+    tabElList.forEach(function(tabEl) {
+      tabEl.addEventListener('click', function (event) {
+        event.preventDefault();
+        var tabTrigger = new window.bootstrap.Tab(tabEl);
+        tabTrigger.show();
+      });
+    });
+  }
 }; 
